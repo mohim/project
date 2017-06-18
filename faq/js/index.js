37b4eda529a2f911addbfad1b0e2ca8f3bd4610b
hasnat mohim <hasnat mohim>
2017-06-18 17:48:26 +0600

    final

---
+++ faq/js/index.js
@@ -0,0 +1,62 @@
+http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+$(document).ready(function(){
+  $("#1").click(function(){
+    $("#2").slideToggle(300);
+    
+      
+  });
+});
+$(document).ready(function(){
+  
+     $("#3").click(function(){
+    $("#4").slideToggle(300);
+      
+  });
+});
+
+$(document).ready(function(){
+  
+     $("#5").click(function(){
+    $("#6").slideToggle(300);
+      
+  });
+});
+
+$(document).ready(function(){
+  
+     $("#7").click(function(){
+    $("#8").slideToggle(300);
+      
+  });
+});
+
+$(document).ready(function(){
+  $("#9").click(function(){
+    $("#10").slideToggle(300);
+    
+      
+  });
+});
+$(document).ready(function(){
+  
+     $("#11").click(function(){
+    $("#12").slideToggle(300);
+      
+  });
+});
+
+$(document).ready(function(){
+  
+     $("#13").click(function(){
+    $("#14").slideToggle(300);
+      
+  });
+});
+
+$(document).ready(function(){
+  
+     $("#15").click(function(){
+    $("#16").slideToggle(300);
+      
+  });
+});
